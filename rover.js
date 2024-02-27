700ae353b97d2a2ef3819467b5633293f58dac19
Tim-E <caffeinated.ink@gmail.com>
2024-02-27 17:31:12 -0500

    GA #3 in-progress commit

--- rover.js
+++ rover.js
@@ -1,15 +1,44 @@
 class Rover {
    // Write code here!
-   constructor(position, mode = 'normal', generatorWatts = 110) {
+   constructor(position, mode = 'NORMAL', generatorWatts = 110) {
       this.position = position,
       this.mode = mode,
       this.generatorWatts = generatorWatts
    }
 
    receiveMessage(message) {
-      let name = message.name;
-      return name;
+      let commands = [message.commands];
+      let results = [];
+      for (let i = 0; i < commands.length; i++) {
+         if (commands[i].commandType === 'MOVE') {
+            if (this.mode === 'LOW_POWER') {
+               results.push({complete: false})
+            } else {
+               this.position = command.value;
+               results.push({completed: true});
+            }
+         } else if (commands[i].commandType === 'MODE_CHANGE') {
+            this.mode = command.value;
+            results.push({complete: true});
+         } else if (commands[i].commandType === 'STATUS_CHECK') {
+            results.push({
+               completed: true,
+               roverStatus: {
+                  mode: this.mode,
+                  generatorWatts: this.generatorWatts,
+                  position: this.position
+               }
+            });
+         }
+      }
+      let response = {
+         name: message.name,
+         results: results
+      };
+      return response;
    }
 }
+
+
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -4,20 +4,47 @@ const Command = require('../command.js');
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
-
+const testCommands = [new Command('MODE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+const singleMoveCommand = new Command('MOVE', 56789);
+const lowPowModeCommand = new Command('MODE_CHANGE', 'LOW_POWER');
+const normalModeCommand = new Command('MODE_CHANGE', 'NORMAL');
+const testMessage = new Message('test_message_name', testCommands);
+const testRover = new Rover(12345);
+const response = testRover.receiveMessage(testMessage);
+const results = response.results[0];
 
 describe("Rover class", function() {
 
   // 7 tests here!
   test("constructor sets position and default values for mode and generatorWatts", function(){
-    let testRover = new Rover(null);
-    expect(testRover.mode).toBe('normal') && expect(testRover.generatorWatts).toBe(110);
+    expect(testRover.mode).toBe('NORMAL') && expect(testRover.generatorWatts).toBe(110);
   });
 
   test("response returned by receiveMessage contains the name of the message", function(){
-    let testMessage = new Message('message test string', null);
-    let testRover = new Rover('12345')
-    expect(testRover.receiveMessage(testMessage)).toBe('message test string');
+    expect(response.name).toBe('test_message_name');
+  });
+
+  test("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
+    // console.log(Object.keys(results[0]).length);
+    expect(Object.keys(results).length).toBe(testCommands.length);
+  });
+
+  test("responds correctly to the status check command", function(){
+    // console.log(results);
+    expect(results.hasOwnProperty('roverStatus')).toBe(true);
+    expect(results.roverStatus.mode).toBe('NORMAL');
+    expect(results.roverStatus.generatorWatts).toBe(110);
+    expect(results.roverStatus.position).toBe(12345);
+  });
+
+  test("responds correctly to the mode change command", function(){
+    // const lowPowMessage = new Message('MODE to LOW_POWER', lowPowModeCommand);
+    // const normalModeMessage = new Message('MODE to NORMAL', normalModeCommand);
+    // const modeTestRover = new Rover(54321);
+    console.log(testRover.receiveMessage(testCommands));
+    expect(results.completed).toBe(true) && expect(testRover.mode).toBe('NORMAL');
+    // expect(testRover.receiveMessage(lowPowModeCommand).results[0])
+    // expect(modeTestRover.receiveMessage(lowPowMessage).results[0].completed).toBe(true);
   });
 
 });
